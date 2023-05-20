603b0605007d89253b494f106075b7fb3b867dcd
SharminTonni <sharmintonni000@gmail.com>
2023-05-20 21:20:33 +0600

    vercel code solved

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-
 const app = express();
 
 const port = process.env.PORT || 5000;
+const corsConfig = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+  //   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+};
 
-app.use(cors());
+app.use(cors(corsConfig));
+app.options("", cors(corsConfig));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.78xjoll.mongodb.net/?retryWrites=true&w=majority`;
